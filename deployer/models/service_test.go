04c4703865fea838f437d9c270ef841b7251fde7
graham jenson <graham.jenson@coinbase.com>
2018-08-02 14:55:47 +0100

    [Fix] SerivceId should be limited to 80 Characters for ASG and LC names

--- deployer/models/service_test.go
+++ deployer/models/service_test.go
@@ -3,7 +3,9 @@ package models
 import (
 	"fmt"
 	"testing"
+	"time"
 
+	"github.com/coinbase/step/bifrost"
 	"github.com/coinbase/step/utils/to"
 	"github.com/stretchr/testify/assert"
 )
@@ -16,4 +18,24 @@ func Test_Service_SetGetUserdata(t *testing.T) {
 	service.SetDefaults(release, "web")
 
 	assert.Equal(t, fmt.Sprintf("%v\n%v\n%v\nweb\n", *release.ReleaseID, *release.ProjectName, *release.ConfigName), *service.UserData())
+}
+
+func Test_Service_ServiceID(t *testing.T) {
+	release := &Release{Release: bifrost.Release{ProjectName: to.Strp("project"), ConfigName: to.Strp("config"), CreatedAt: &time.Time{}}}
+	service := &Service{
+		release:     release,
+		ServiceName: to.Strp("service"),
+	}
+
+	assert.Equal(t, *service.ServiceID(), "project-config-0001-01-01T00-00-00Z-service")
+
+	service.ServiceName = to.Strp("this_will_cause_a_name_longer_than_80_characters")
+	assert.Equal(t, *service.ServiceID(), "project-co-0001-01-01T00-00-00Z-this_will_cause_a_name_longer_than_80_characters")
+	assert.Equal(t, len(*service.ServiceID()), 80)
+
+	// This should not happen due to Char limit on ServiceName, but still should not error
+	service.ServiceName = to.Strp("this_will_cause_a_name_longer_than_80_characters_which_is_longer_than_the_max_crazzy")
+	assert.Equal(t, *service.ServiceID(), "project-config-0001-01-01T00-00-00Z-this_will_cause_a_name_longer_than_80_charac")
+	assert.Equal(t, len(*service.ServiceID()), 80)
+
 }
